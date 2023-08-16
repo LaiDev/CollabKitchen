5f25b73ef181feda74b600ffaa552a96bafe0819
Lai <l.bnjamin0309@gmail.com>
2023-08-16 15:05:15 -0400

    ADD: Creates recipe object with inputted form data

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,31 +1,14 @@
 //A class storing all of the ingredients and directions for the recipe. Including Recipe Name, Cook Time, Serving Size, Ingredents, & Directions
 class createRecipe {
-    constructor(recipeName, recipeAuthor, cookTime, servingSize){
+    constructor(recipeName, recipeAuthor, cookTime, servingSize, ingredientsArr, directionsArr){
         this.recipeName = recipeName;
         this.recipeAuthor = recipeAuthor;
         this.cookTime = cookTime;
         this.servingSize = servingSize;
-        this.ingredientsArr = [];
-        this.directionsArr = [];
+        this.ingredientsArr = ingredientsArr;
+        this.directionsArr = directionsArr;;
     }
 
-   //Method to add ingredients to the ingredients Array
-    addIngredients(ingredient){
-           this.ingredientsArr.push(ingredient);
-    }
-
-    //Method to add directions to the directions Array
-    addDirections(directions){
-        this.directionsArr.push(directions);
-    }
-
-    checkIngredients(){
-        console.log(this.ingredientsArr);
-    }
-
-    checkDirections(){
-        console.log(this.directionsArr);
-    }
 }
 
 //Testing Recipe and methods
@@ -39,28 +22,54 @@ console.log(testRecipe)
 const ingredientField = document.getElementById("ingredients");
 const ingredientBtn = document.getElementById("addIngredientBtn");
 const ingredientList = document.getElementById("ingredentList");
+const ingredientArr = [];
 
 ingredientBtn.addEventListener("click", () => {
-    event.preventDefault();
-    console.log(ingredientField.value);
-
+    e.preventDefault();
+    if(ingredientField.value != "")
+    {
     const ingredient = document.createElement("li");
     ingredient.innerHTML = ingredientField.value;
+    ingredient.classList.add("ingredients");
     ingredientList.append(ingredient);
+    ingredientArr.push(ingredient.innerHTML);
     ingredientField.value = "";
+    }
+
 })
 
 //Listens for when adding a step, and displays it in the form
 const directionField = document.getElementById("directions");
 const directionBtn = document.getElementById("addDirectionsBtn");
 const directionList = document.getElementById("directionsList");
+const directionArr = [];
 
 directionBtn.addEventListener("click", () => {
-    event.preventDefault();
-    console.log(directionField.value);
-
+    e.preventDefault();
+    if(directionField.value != "")
+    {
     const direction = document.createElement("li");
     direction.innerHTML = directionField.value;
     directionList.append(direction);
+    directionArr.push(direction.innerHTML);
     directionField.value = "";
+    }
+})
+
+const recipeName = document.getElementById("recipeName");
+const recipeAuthor = document.getElementById("recipeAuthor")
+const cookTime = document.getElementById("cookTime");
+const servingSize = document.getElementById("servingSize");
+
+//Gets all the information from the form fields and passes it into the createRecipe class to create a new recipe Object
+const createRecipeBtn = document.getElementById("createRecipe");
+createRecipeBtn.addEventListener("click", () => {
+    e.preventDefault();
+    console.log(recipeName.value);
+    console.log(recipeAuthor.value);
+    console.log(cookTime.value);
+    console.log(servingSize.value);
+    let recipe = new createRecipe(recipeName.value,recipeAuthor.value,cookTime.value,servingSize.value, ingredientArr, directionArr);
+    console.log(recipe)
+
 })
